4052bef9654ce0e2eddbdf704f0451803c98af78
Linden <linden.quan2020@outlook.com>
2024-10-13 15:12:40 -0300

    fix(app): test release action

--- packages/app/src/sum.test.ts
+++ packages/app/src/sum.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, it } from "vitest";
 
 describe("sum function", () => {
   it("should return the sum of two numbers", () => {
-    expect(1 + 2).toBe(3);
+    expect(1 + 1).toBe(2);
   });
 });
