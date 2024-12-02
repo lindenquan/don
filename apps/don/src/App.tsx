28a755cc4a23f9677f467d4c6a0dc7805494e621
Linden <linden.quan2020@outlook.com>
2024-12-02 00:49:44 -0400

    chore: test 3: changeset

--- apps/don/src/App.tsx
+++ apps/don/src/App.tsx
@@ -17,7 +17,7 @@ const App: Component = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solid & changesets !
+          Learn Solid & changesets
         </a>
       </header>
     </div>
