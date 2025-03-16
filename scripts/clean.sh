3036d1bfcb937460c79731011d65a3855f1d1680
Linden <linden.quan2020@outlook.com>
2025-03-16 00:30:35 -0300

    chore: update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 rm -rf .turbo node_modules
+
+# Define the directories to delete
+TARGET_DIRS=("node_modules" "dist" "coverage" ".turbo")
+
+# Loop through each target directory name
+for dir in "${TARGET_DIRS[@]}"; do
+  # Find and delete directories
+  find . -type d -name "$dir" -prune -exec rm -rf {} + 
+  
+  echo "Deleted all '$dir' directories."
+done
+
+find . -type f -name "tsconfig.tsbuildinfo" -prune -exec rm -rf {} + 
+
+echo "Cleanup complete."
