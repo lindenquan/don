3154208364702f6aabf350c83ab5dadc9fe9032e
Linden <linden.quan2020@outlook.com>
2025-03-18 17:18:27 -0300

    chore: update configs

--- apps/don/src/App.tsx
+++ apps/don/src/App.tsx
@@ -1,32 +1,21 @@
+import { Route, Router } from "@solidjs/router";
 import type { Component } from "solid-js";
-import { createEffect } from "solid-js";
+import { Suspense, createEffect, lazy } from "solid-js";
 
-import logo from "~/public/images/logo.svg";
-import styles from "~/src/App.module.css";
+const Home = lazy(() => import("./pages/home/Home"));
+const About = lazy(() => import("./pages/about/About"));
 
 const App: Component = () => {
-  console.log(import.meta.env);
-
   createEffect(() => {
     document.title = import.meta.env.VITE_APP_TITLE;
   });
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid & changesets
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Router>
+    </Suspense>
   );
 };
 
