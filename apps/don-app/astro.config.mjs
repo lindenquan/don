a51c8aad40a0292dd9cfe8bfa87146327e258dbb
Linden <linden.quan2020@outlook.com>
2025-05-09 15:12:54 -0300

    feat: use Astro

--- apps/don-app/astro.config.mjs
+++ apps/don-app/astro.config.mjs
@@ -1,11 +1,14 @@
 import path from "node:path";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
+// @ts-check
+import { defineConfig } from "astro/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
